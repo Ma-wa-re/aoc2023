33bfac33fc83d44aca387342e20ca619d20a514a
Jake Richardson (Ma-wa-re) <jakey8180+github@gmail.com>
2023-12-06 13:45:25 +0000

    Day 4 part 2

--- src/day_4/mod.rs
+++ src/day_4/mod.rs
@@ -3,14 +3,18 @@ use std::collections::HashSet;
 pub fn run() {
     let games: Vec<Game> = parse_input();
     println!("Part 1");
-    part_1(&games)
+    part_1(&games);
+    
+    println!("Part 2");
+    part_2(&games)
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Game {
     number: usize,
     winning_numbers: HashSet<usize>,
-    player_numbers: HashSet<usize>
+    player_numbers: HashSet<usize>,
+    count: usize
 }
 
 impl Game {
@@ -27,6 +31,10 @@ impl Game {
         } else {
             0
         }
+    }
+
+    fn number_of_wins(&self) -> usize {
+        self.winning_numbers.intersection(&self.player_numbers).count()
     }
 }
 
@@ -53,6 +61,7 @@ fn parse_input() -> Vec<Game>{
         
         games.push(Game { 
             number: game_lines.0+1,
+            count: 1,
             winning_numbers,
             player_numbers
         });
@@ -65,9 +74,30 @@ fn part_1(games: &Vec<Game>) {
     let mut total_points: usize = 0;
 
     for game in games {
-        println!("Game {}: {}", game.number, game.points());
+        // println!("Game {}: {}", game.number, game.points());
         total_points += game.points()
     }
 
     println!("Total points: {}", total_points);
+}
+
+fn part_2(games: &Vec<Game>) {
+    let mut games_mut = games.to_vec();
+    
+    for game in games.iter().enumerate() {
+        let num_of_wins: usize = game.1.number_of_wins();
+
+        if num_of_wins > 0 {
+            for i in 1..num_of_wins+1 {
+                games_mut[game.0+i].count += 1*games_mut[game.0].count;
+            }
+        }
+    }
+
+    let mut total: usize = 0;
+    for game in games_mut {
+        total += game.count;
+    }
+
+    println!("Number of cards: {}", total);
 }
